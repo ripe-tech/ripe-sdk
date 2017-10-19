1dbb2cfed75e07366f1f14abc8687cfba40a825c
joamag <joamag@gmail.com>
2017-10-19 16:16:39 +0100

    removed file

--- src/js/interactable/interactable.part.js
+++
@@ -1,9 +0,0 @@
-Ripe.Interactable = function(owner, options) {
-    this.owner = owner;
-    this.options = options || {};
-    this.init();
-};
-
-Ripe.Interactable.prototype.init = function() { };
-
-Ripe.Interactable.prototype.update = function(state) {};
