28f6f54231091f8e885a8e6f10ed9cf8f034b4f3
joamag <joamag@gmail.com>
2017-11-14 23:23:23 +0000

    small fix

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ gulp.task("compress", ["build-js"], () => {
 gulp.task("mark", () => {
     return gulp.src(paths.scripts)
         .pipe(replace("__VERSION__", _package.version))
-        .pipe(gulp.dest("src"));
+        .pipe(gulp.dest("src/js"));
 });
 
 gulp.task("test", () => {
