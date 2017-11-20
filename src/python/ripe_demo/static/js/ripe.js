4571350304e01df08b75ed12996c60e0f9dcbca2
Marcos Oliveira <mo@platforme.com>
2017-11-20 09:59:14 +0000

    renamed highlight/lowlight

--- src/python/ripe_demo/static/js/ripe.js
+++ src/python/ripe_demo/static/js/ripe.js
@@ -635,11 +635,11 @@ ripe.Configurator.prototype.init = function() {
     });
 
     this.owner.bind("selected_part", function(part) {
-        this.highlightPart(part);
+        this.highlight(part);
     }.bind(this));
 
     this.owner.bind("deselected_part", function(part) {
-        this.lowlightPart();
+        this.lowlight();
     }.bind(this));
 };
 
@@ -816,7 +816,7 @@ ripe.Configurator.prototype.changeFrame = function(frame, options) {
     });
 };
 
-ripe.Configurator.prototype.highlightPart = function(part, options) {
+ripe.Configurator.prototype.highlight = function(part, options) {
     // verifiers if masks are meant to be used for the current model
     // and if that's not the case returns immediately
     if (!this.useMasks) {
@@ -880,7 +880,7 @@ ripe.Configurator.prototype.highlightPart = function(part, options) {
     this.element.classList.add("highlight");
 };
 
-ripe.Configurator.prototype.lowlightPart = function(options) {
+ripe.Configurator.prototype.lowlight = function(options) {
     // verifiers if masks are meant to be used for the current model
     // and if that's not the case returns immediately
     if (!this.useMasks) {
@@ -1254,7 +1254,7 @@ ripe.Configurator.prototype._registerHandlers = function() {
     // registes for the selected part event on the owner
     // so that we can highlight the associated part
     this.owner.bind("selected_part", function(part) {
-        this.highlightPart(part);
+        this.highlight(part);
     }.bind(this));
 
     // binds the mousedown event on the element to prepare
@@ -1334,14 +1334,14 @@ ripe.Configurator.prototype._registerHandlers = function() {
         // position and the associated operation is the removal of the highlight
         // also if the target is being dragged the highlight should be removed
         if (index === 0 || self.down === true) {
-            self.lowlightPart();
+            self.lowlight();
             return;
         }
 
         // retrieves the reference to the part name by using the index
         // extracted from the masks image (typical strategy for retrieval)
         var part = self.partsList[index - 1];
-        self.hiddenParts.indexOf(part) === -1 && self.highlightPart(part);
+        self.hiddenParts.indexOf(part) === -1 && self.highlight(part);
     });
 
     area.addEventListener("dragstart", function(event) {
@@ -1380,14 +1380,14 @@ ripe.Configurator.prototype._registerHandlers = function() {
         // position and the associated operation is the removal of the highlight
         // also if the target is being dragged the highlight should be removed
         if (index === 0 || self.down === true) {
-            self.lowlightPart();
+            self.lowlight();
             return;
         }
 
         // retrieves the reference to the part name by using the index
         // extracted from the masks image (typical strategy for retrieval)
         var part = self.partsList[index - 1];
-        self.hiddenParts.indexOf(part) === -1 && self.highlightPart(part);
+        self.hiddenParts.indexOf(part) === -1 && self.highlight(part);
     });
 
     back.addEventListener("dragstart", function(event) {
