20d9e4253af458fc5e5413a303350eb87fcff723
Afonso Caldas <anc@ripe.tech>
2018-02-09 11:32:14 +0000

    code fix

--- src/python/ripe_demo/static/js/ripe.js
+++ src/python/ripe_demo/static/js/ripe.js
@@ -848,6 +848,10 @@ ripe.Configurator.prototype.highlight = function(part, options) {
     var maskOpacity = this.element.dataset.mask_opacity || this.maskOpacity;
     var maskDuration = this.element.dataset.mask_duration || this.maskDuration;
 
+    // adds the highlight class to the current target configurator meaning
+    // that the front mask is currently active and showing info
+    this.element.classList.add("highlight");
+
     // constructs the full URL of the mask image that is going to be
     // set for the current highlight operation (to be determined)
     var url = this.owner._getMaskURL({
@@ -862,10 +866,8 @@ ripe.Configurator.prototype.highlight = function(part, options) {
     var frontMask = this.element.querySelector(".front-mask");
     var src = frontMask.getAttribute("src");
     if (src === url) {
-        this.element.classList.add("highlight");
         return;
     }
-
     var frontMaskLoad = function() {
         this.classList.add("loaded");
         this.classList.add("highlight");
@@ -881,10 +883,6 @@ ripe.Configurator.prototype.highlight = function(part, options) {
     var animationId = frontMask.dataset.animation_id;
     cancelAnimationFrame(animationId);
     ripe.animateProperty(frontMask, "opacity", 0, maskOpacity, maskDuration);
-
-    // adds the highlight class to the current target configurator meaning
-    // that the front mask is currently active and showing info
-    this.element.classList.add("highlight");
 };
 
 ripe.Configurator.prototype.lowlight = function(options) {
