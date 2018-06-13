baac753f343bc82f46b5a20082015187f1355207
joamag <joamag@hive.pt>
2018-06-13 19:20:12 +0100

    new eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,3 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": ["mocha"],
-    "rules": {
-        "indent": ["warn", 4, {
-            SwitchCase: 1
-        }],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-        "space-before-function-paren": ["error", {
-            "anonymous": "never",
-            "named": "never",
-            "asyncArrow": "always"
-        }],
-        "linebreak-style": ["error", "windows"],
-        "mocha/no-exclusive-tests": "error",
-        "standard/no-callback-literal": "off"
-    },
-    "env": {
-        "browser": true,
-        "jasmine": true
-    }
+    "extends": "hive"
 };
