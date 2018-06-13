2ce9647dfac60b93e61c092917fea054c6329ec9
Afonso Caldas <anc@ripe.tech>
2018-06-13 16:41:07 +0100

    new sync plugin

--- src/python/ripe_demo/static/js/ripe.js
+++ src/python/ripe_demo/static/js/ripe.js
@@ -1189,7 +1189,7 @@ if (typeof window === "undefined" && typeof require !== "undefined") {
 
 ripe.Ripe.plugins.SyncPlugin = function(rules, options) {
     options = options || {};
-    this.rules = rules;
+    this.rules = this._buildRules(rules);
     this.partCallback = this._applySync.bind(this);
 };
 
@@ -1213,24 +1213,69 @@ ripe.Ripe.plugins.SyncPlugin.prototype.unregister = function(owner) {
     ripe.Ripe.plugins.Plugin.prototype.unregister.call(this, owner);
 };
 
+ripe.Ripe.plugins.SyncPlugin.prototype._buildRules = function(rules) {
+    var _rules = {};
+    for (var ruleName in rules) {
+        var rule = rules[ruleName];
+        for (var index = 0; index < rule.length; index++) {
+            var part = rule[index];
+            if (typeof part === "string") {
+                part = {
+                    part: part
+                };
+                rule[index] = part;
+            }
+        }
+        _rules[ruleName] = rule;
+    }
+    return _rules;
+};
+
 ripe.Ripe.plugins.SyncPlugin.prototype._applySync = function(name, value) {
     for (var key in this.rules) {
         // if a part was selected and it is part of
         // the rule then its value is used otherwise
         // the first part of the rule is used
         var rule = this.rules[key];
-        var part = name && rule.indexOf(name) !== -1 ? name : rule[0];
-        var partValue = this.owner.parts[part];
+        var firstPart = rule[0];
+        name = name || firstPart.part;
+        value = value || this.owner.parts[name];
+
+        // checks if the part triggers the sync rule
+        // and skips to the next rule if it doesn't
+        if (this._shouldSync(rule, name, value) === false) {
+            continue;
+        }
 
         // iterates through the parts of the rule and
-        // sets their material and color to be the same
-        // of the reference part
+        // sets their material and color according to
+        // the sync rule
         for (var index = 0; index < rule.length; index++) {
             var _part = rule[index];
-            this.owner.parts[_part].material = partValue.material;
-            this.owner.parts[_part].color = partValue.color;
+            if (_part.part === name) {
+                continue;
+            }
+            if (_part.color === undefined) {
+                this.owner.parts[_part.part].material = _part.material ? _part.material : value.material;
+            }
+            this.owner.parts[_part.part].color = _part.color ? _part.color : value.color;
         }
     }
+};
+
+ripe.Ripe.plugins.SyncPlugin.prototype._shouldSync = function(rule, name, value) {
+    for (var index = 0; index < rule.length; index++) {
+        var rulePart = rule[index];
+        var part = rulePart.part;
+        var material = rulePart.material;
+        var color = rulePart.color;
+        var materialSync = !material || material === value.material;
+        var colorSync = !color || color === value.color;
+        if (part === name && materialSync && colorSync) {
+            return true;
+        }
+    }
+    return false;
 };
 
 if (typeof window === "undefined" && typeof require !== "undefined") {
