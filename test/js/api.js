561cab0f5ab485e2180c3a15505f9f10a2f8436c
joamag <joamag@gmail.com>
2018-07-09 08:38:06 +0100

    better unit testing

--- test/js/api.js
+++ test/js/api.js
@@ -1,11 +1,13 @@
 const assert = require("assert");
 const config = require("./config");
+const ripe = require("../../src/js");
 
 describe("API", function() {
     this.timeout(config.TEST_TIMEOUT);
 
     describe("#getSizes", function() {
         it("should be able to retrieve sizes", () => {
+            const remote = ripe.RipeAPI();
         });
     });
 });
