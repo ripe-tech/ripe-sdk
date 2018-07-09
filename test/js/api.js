e92922dcc9d87500710d1fb69edaef96a1d63913
joamag <joamag@gmail.com>
2018-07-09 08:33:24 +0100

    initial testing of api

---
+++ test/js/api.js
@@ -0,0 +1,11 @@
+const assert = require("assert");
+const config = require("./config");
+
+describe("API", function() {
+    this.timeout(config.TEST_TIMEOUT);
+
+    describe("#getSizes", function() {
+        it("should be able to retrieve sizes", () => {
+        });
+    });
+});
