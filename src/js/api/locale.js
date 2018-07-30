7aef6e2d9caebc3ef57dea08b802b2296e5d1a81
Afonso Caldas <anc@ripe.tech>
2018-07-30 15:15:15 +0100

    added getLocaleMultiple

--- src/js/api/locale.js
+++ src/js/api/locale.js
@@ -5,8 +5,12 @@ if (typeof require !== "undefined") {
     var ripe = base.ripe;
 }
 
-ripe.Ripe.prototype.getLocale = function(values, locale, options, callback) {
-    values = typeof options === "string" ? [values] : values;
+ripe.Ripe.prototype.getLocale = function(value, locale, options, callback) {
+    return this.getLocaleMultiple(value, locale, options, callback);
+};
+
+ripe.Ripe.prototype.getLocaleMultiple = function(values, locale, options, callback) {
+    values = typeof values === "string" ? [values] : values;
     callback = typeof options === "function" ? options : callback;
     options = typeof options === "function" ? {} : options;
     var url = this.url + "locale";
