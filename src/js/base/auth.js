c250bbb6ffd8e9a45a34ec7a0b12968ce4be4293
Afonso Caldas <anc@ripe.tech>
2019-04-05 18:05:58 +0100

    Added promise version of authPidP

--- src/js/base/auth.js
+++ src/js/base/auth.js
@@ -75,6 +75,14 @@ ripe.Ripe.prototype.authPid = function(token, options, callback) {
     );
 };
 
+ripe.Ripe.prototype.authPidP = function(token, options) {
+    return new Promise((resolve, reject) => {
+        this.authPid(token, options, (result, isValid, request) => {
+            isValid ? resolve(result) : reject(new Error());
+        });
+    });
+};
+
 ripe.Ripe.prototype.unauth = function(options, callback) {
     this.sid = null;
 
