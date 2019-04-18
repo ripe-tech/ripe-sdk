e47a6262896834c2a872e72902d3431eece9fd31
Hugo <hg@platforme.com>
2019-04-18 11:32:10 +0100

    Added documentation to Image

--- src/js/visual/image.js
+++ src/js/visual/image.js
@@ -9,12 +9,37 @@ if (typeof require !== "undefined") {
         typeof WebKitMutationObserver === "undefined" ? null : WebKitMutationObserver;
 }
 
+/**
+ * @class
+ * @classdesc Class that reactively updates the image of an img element whenever
+ * the state of its owner changes.
+ * An Image can be configured with the following options:
+ *
+ * @param {Ripe} owner The Ripe instance to be shown.
+ * @param {Object} element The img element that should be updated.
+ * @param {Object} options An object with options to configure the image:
+ * - 'showInitials' - A Boolean indicating if the owner's personalization should be shown (defaults to 'true').
+ * - 'initialsBuilder' - A function that receives the initials and engraving as Strings and the img element that
+ * will be used and returns a map with the initials and a profile list.
+ * - 'frame' - The Ripe instance frame to display (defaults to '0').
+ * - 'size' - The image size in pixels (defaults to '1000').
+ * - 'width' - The image width in pixels (defaults to 'null', meaning it will fallback to 'size').
+ * - 'height' - The image height in pixels (defaults to 'null', meaning it will fallback to 'size').
+ * - 'crop' - A Boolean indicating if it is to crop the image composition.
+ */
 ripe.Image = function(owner, element, options) {
     ripe.Visual.call(this, owner, element, options);
 };
 
 ripe.Image.prototype = ripe.build(ripe.Visual.prototype);
 
+/**
+ * The Image initializer, which is called (by the owner)
+ * whenever the Image is going to become active.
+ *
+ * Sets the various values for the Image taking into
+ * owner's default values.
+ */
 ripe.Image.prototype.init = function() {
     ripe.Visual.prototype.init.call(this);
 
@@ -37,6 +62,12 @@ ripe.Image.prototype.init = function() {
     this._registerHandlers();
 };
 
+/**
+ * This function is called (by the owner) whenever its state changes
+ * so that the Image can update itself for the new state.
+ *
+ * @param {Object} state An object containing the new state of the owner.
+ */
 ripe.Image.prototype.update = function(state) {
     const frame = this.element.dataset.frame || this.frame;
     const size = this.element.dataset.size || this.size;
@@ -78,6 +109,11 @@ ripe.Image.prototype.update = function(state) {
     this.element.src = url;
 };
 
+/**
+ * The Image deinitializer, to be called (by the owner) when
+ * it should stop responding to updates so that any necessary
+ * cleanup operations can be executed.
+ */
 ripe.Image.prototype.deinit = function() {
     this._unregisterHandlers();
     this._observer = null;
@@ -86,16 +122,34 @@ ripe.Image.prototype.deinit = function() {
     ripe.Visual.prototype.deinit.call(this);
 };
 
+/**
+ * Updates the frame that the Image is referring to.
+ *
+ * @param {String} frame The Ripe instance frame to display.
+ * @param {Object} options An object with options to configure
+ * the setting of the frame.
+ */
 ripe.Image.prototype.setFrame = function(frame, options) {
     this.frame = frame;
     this.update();
 };
 
+/**
+ * Updates the Image's 'initialsBuilder' function.
+ *
+ * @param {Function} builder The new 'initialsBuilder' function
+ * to be used by the Image.
+ * @param {Object} options An object with options to configure
+ * the setting of the 'initialsBuilder'.
+ */
 ripe.Image.prototype.setInitialsBuilder = function(builder, options) {
     this.initialsBuilder = builder;
     this.update();
 };
 
+/**
+ * @ignore
+ */
 ripe.Image.prototype._registerHandlers = function() {
     this.loadListener = function() {
         this.trigger("loaded");
@@ -118,6 +172,9 @@ ripe.Image.prototype._registerHandlers = function() {
         });
 };
 
+/**
+ * @ignore
+ */
 ripe.Image.prototype._unregisterHandlers = function() {
     this.element.removeEventListener("load", this.loadListener);
     this._observer && this._observer.disconnect();
