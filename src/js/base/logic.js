64bc17062bc68f81b82bdd664804955e4e1ee835
joamag <joamag@gmail.com>
2019-06-17 11:43:36 +0100

    Added logic.js

---
+++ src/js/base/logic.js
@@ -0,0 +1,9 @@
+if (typeof require !== "undefined") {
+    // eslint-disable-next-line no-redeclare
+    var base = require("./base");
+    // eslint-disable-next-line no-redeclare
+    var ripe = base.ripe;
+}
+
+ripe.parseEngraving = function() {
+};
