f68a61c928b7321bf9ebb82ca4d6eb06b4aa69c9
joamag <joamag@hive.pt>
2019-07-04 19:07:48 +0100

    New api methods for locale

--- src/js/api/locale.js
+++ src/js/api/locale.js
@@ -18,6 +18,14 @@ ripe.Ripe.prototype.locale = function(value, locale, options, callback) {
     return this.localeMultiple(value, locale, options, callback);
 };
 
+ripe.Ripe.prototype.localeP = function(value, locale, options) {
+    return new Promise((resolve, reject) => {
+        this.locale(value, locale, options, (result, isValid, request) => {
+            isValid ? resolve(result) : reject(new ripe.RemoteError(request));
+        });
+    });
+};
+
 /**
  * Localizes a list of values to the provided locale.
  *
@@ -42,4 +50,45 @@ ripe.Ripe.prototype.localeMultiple = function(values, locale, options, callback)
     });
     options = this._build(options);
     return this._cacheURL(options.url, options, callback);
+};
+
+ripe.Ripe.prototype.localeMultipleP = function(values, locale, options) {
+    return new Promise((resolve, reject) => {
+        this.localeMultiple(values, locale, options, (result, isValid, request) => {
+            isValid ? resolve(result) : reject(new ripe.RemoteError(request));
+        });
+    });
+};
+
+/**
+ * Retrieves a bundle of locales for the provided locale value.
+ *
+ * @param {String} locale The locale string to retrieve the bundle.
+ * @param {String} context The inner context for the locale bundle.
+ * @param {Object} options An object of options to configure the request.
+ * @param {Function} callback Function with the result of the request.
+ * @returns {XMLHttpRequest} The XMLHttpRequest instance of the API request.
+ */
+ripe.Ripe.prototype.localeBundle = function(locale, context, options, callback) {
+    callback = typeof options === "function" ? options : callback;
+    options = typeof options === "function" || options === undefined ? {} : options;
+    const url = this.url + "locale/bundle";
+    options = Object.assign(options, {
+        url: url,
+        method: "GET",
+        params: {
+            locale: locale,
+            context: context
+        }
+    });
+    options = this._build(options);
+    return this._cacheURL(options.url, options, callback);
+};
+
+ripe.Ripe.prototype.localeBundleP = function(locale, context, options) {
+    return new Promise((resolve, reject) => {
+        this.localeBundle(locale, context, options, (result, isValid, request) => {
+            isValid ? resolve(result) : reject(new ripe.RemoteError(request));
+        });
+    });
 };
