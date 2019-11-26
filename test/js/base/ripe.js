9d48fcec9db8b91aac5da96c6c68eaa9a9583d16
joamag <joamag@hive.pt>
2019-11-26 13:10:45 +0000

    Fixed regression

--- test/js/base/ripe.js
+++ test/js/base/ripe.js
@@ -97,6 +97,14 @@ describe("Ripe", function() {
             assert.strictEqual(instance.initialsExtra.side.initials, "CR9");
             assert.strictEqual(instance.initialsExtra.side.engraving, null);
         });
+    });
+
+    describe("#setInitials (rejects)", async function() {
+        beforeEach(function() {
+            if (!assert.rejects) {
+                this.skip();
+            }
+        });
 
         it("should be able to validate invalid states", async () => {
             const instance = await new ripe.Ripe({ init: false });
