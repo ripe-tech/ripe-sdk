77ded2af06c7fca214344ec31cd521bd8f765ada
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-16 17:12:58 +0000

    Documentation

--- src/js/api/justification.js
+++ src/js/api/justification.js
@@ -9,8 +9,20 @@ if (
 }
 
 /**
- * Gets the Justifications list
- * Can be filtered by context
+ * Gets the justifications list. Can be optionally filtered by context.
+ *
+ * @param {Object} options An object of options to configure the query and
+ * its results, such as:
+ * - 'filters[]' - List of filters that the query will use to, operators such as
+ * ('in', 'not_in', 'like', 'likei', 'llike', 'llikei', 'rlike', 'rlikei', 'contains'),
+ * (eg: 'context:eq:42') would filter by the 'context' that equals to '42'.
+ * - 'sort' - List of arguments to sort the results by and which direction
+ * to sort them in (eg: 'id:ascending') would sort by the id attribute in ascending order,
+ * while (eg: 'id:descending')] would do it in descending order.
+ * - 'skip' - The number of the first record to retrieve from the results.
+ * - 'limit' - The number of results to retrieve.
+ * @param {Function} callback Function with the result of the request.
+ * @returns {XMLHttpRequest} The XMLHttpRequest instance of the API request.
  */
 ripe.Ripe.prototype.getJustifications = function(context = null, options, callback) {
     callback = typeof options === "function" ? options : callback;
@@ -28,9 +40,11 @@ ripe.Ripe.prototype.getJustifications = function(context = null, options, callba
 };
 
 /**
- * Gets the Justifications list
- * Can be filtered by context
- * returns promise
+ * Gets the justifications list. Can be optionally filtered by context.
+ *
+ * @param {String} context The context of the justifications to find by.
+ * @param {Object} options An object of options to configure the request.
+ * @returns {Promise} The orders result list.
  */
 ripe.Ripe.prototype.getJustificationsP = function(context = null, options) {
     return new Promise((resolve, reject) => {
