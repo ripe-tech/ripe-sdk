b5bfbeea3d1ed229d597d0993e2dbee81395c777
Nuno Silva <nuno_nfss96@hotmail.com>
2019-12-18 10:13:51 +0000

    Updated getJustifications

--- src/js/api/justification.js
+++ src/js/api/justification.js
@@ -9,13 +9,12 @@ if (
 }
 
 /**
- * Gets the justifications list. Can be optionally filtered by context.
+ * Provides a list of all the available justifications. Optionally filtered by a set of options.
  *
- * @param {Object} options An object of options to configure the query and
+ * @param {Object} options An object of options to configure the request.
  * its results, such as:
  * - 'filters[]' - List of filters that the query will use to, operators such as
- * ('in', 'not_in', 'like', 'likei', 'llike', 'llikei', 'rlike', 'rlikei', 'contains'),
- * (eg: 'context:eq:42') would filter by the 'context' that equals to '42'.
+ * ('in', 'not_in', 'like', 'contains'), for instance (eg: 'id:eq:42') would filter by the id that equals to 42.
  * - 'sort' - List of arguments to sort the results by and which direction
  * to sort them in (eg: 'id:ascending') would sort by the id attribute in ascending order,
  * while (eg: 'id:descending')] would do it in descending order.
@@ -24,12 +23,10 @@ if (
  * @param {Function} callback Function with the result of the request.
  * @returns {XMLHttpRequest} The XMLHttpRequest instance of the API request.
  */
-ripe.Ripe.prototype.getJustifications = function(context = null, options, callback) {
+ripe.Ripe.prototype.getJustifications = function(options, callback) {
     callback = typeof options === "function" ? options : callback;
     options = typeof options === "function" || options === undefined ? {} : options;
-
-    let url = this.url + "justifications";
-    if (context) url += "/" + context;
+    const url = this.url + "justifications";
     options = Object.assign(options, {
         url: url,
         method: "GET",
@@ -40,15 +37,14 @@ ripe.Ripe.prototype.getJustifications = function(context = null, options, callba
 };
 
 /**
- * Gets the justifications list. Can be optionally filtered by context.
+ * Gets the justifications list of all the available justifications. Optionally filtered by a set of options.
  *
- * @param {String} context The context of the justifications to find by.
  * @param {Object} options An object of options to configure the request.
  * @returns {Promise} The orders result list.
  */
-ripe.Ripe.prototype.getJustificationsP = function(context = null, options) {
+ripe.Ripe.prototype.getJustificationsP = function(options) {
     return new Promise((resolve, reject) => {
-        this.getJustifications(context, options, (result, isValid, request) => {
+        this.getJustifications(options, (result, isValid, request) => {
             isValid ? resolve(result) : reject(new ripe.RemoteError(request));
         });
     });
