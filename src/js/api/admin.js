eea8e35bb40b5f0e97a64c1b34235f5126ec3ff0
joamag <joamag@gmail.com>
2020-02-18 16:41:58 +0000

    New build artifact support

--- src/js/api/admin.js
+++ src/js/api/admin.js
@@ -1,6 +1,9 @@
 if (
     typeof require !== "undefined" &&
-    (typeof window === "undefined" || typeof __webpack_require__ !== "undefined") // eslint-disable-line camelcase
+    (typeof window === "undefined" ||
+        // eslint-disable-next-line camelcase
+        typeof __webpack_require__ !== "undefined" ||
+        (navigator !== undefined && navigator.product === "ReactNative"))
 ) {
     // eslint-disable-next-line no-redeclare
     var base = require("../base");
@@ -9,14 +12,14 @@ if (
 }
 
 /**
- * Retrieves the build information by brand name and version.
+ * Retrieves the build artifact information by brand name and version.
  *
- * @param {String} name The name of the brand of the build.
- * @param {Number} version The number of the version of the build.
+ * @param {String} name The name of the brand of the build artifact.
+ * @param {Number} version The number of the version of the build artifact.
  * @param {Object} options An object of options to configure the request.
  * @returns {XMLHttpRequest} The XMLHttpRequest instance of the API request.
  */
-ripe.Ripe.prototype.getBuildAdmin = function(name, version, options, callback) {
+ripe.Ripe.prototype.getBuildArtifactAdmin = function(name, version, options, callback) {
     callback = typeof options === "function" ? options : callback;
     options = typeof options === "function" || options === undefined ? {} : options;
     const url = `${this.webUrl}admin/builds/${name}/artifacts/${version}`;
@@ -30,14 +33,14 @@ ripe.Ripe.prototype.getBuildAdmin = function(name, version, options, callback) {
 };
 
 /**
- * Retrieves the build information by brand name and version.
+ * Retrieves the build artifact information by brand name and version.
  *
- * @param {String} name The name of the brand of the build.
- * @param {Number} version The number of the version of the build.
+ * @param {String} name The name of the brand of the build artifact.
+ * @param {Number} version The number of the version of the build artifact.
  * @param {Object} options An object of options to configure the request.
  * @returns {Promise} The build result (as a promise).
  */
-ripe.Ripe.prototype.getBuildAdminP = function(name, version, options) {
+ripe.Ripe.prototype.getBuildArtifactAdminP = function(name, version, options) {
     return new Promise((resolve, reject) => {
         this.getBuild(name, version, options, (result, isValid, request) => {
             isValid ? resolve(result) : reject(new ripe.RemoteError(request));
