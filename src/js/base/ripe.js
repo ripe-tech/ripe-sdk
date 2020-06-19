b71c4399c2bceaf01e3efb298c6cf0a27d4cf481
Nuno Silva <nuno_nfss96@hotmail.com>
2020-06-19 21:15:05 +0100

    Added messages event

--- src/js/base/ripe.js
+++ src/js/base/ripe.js
@@ -1638,6 +1638,7 @@ ripe.Ripe.prototype._handleCtx = function(result) {
     if (result.choices && !ripe.equal(result.choices, this.choices)) {
         this.setChoices(result.choices);
     }
+    this.trigger("messages", result.messages);
     for (const [name, value] of result.messages) {
         this.trigger("message", name, value);
     }
