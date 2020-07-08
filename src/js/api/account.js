fc8bf0cc57f5f0ccc82a7a5598e82de9dda02abc
3rdvision <kazgru@gmail.com>
2020-07-08 11:53:15 +0100

    Fix accountMe method name

--- src/js/api/account.js
+++ src/js/api/account.js
@@ -40,7 +40,7 @@ ripe.Ripe.prototype.accountMe = function(options, callback) {
  */
 ripe.Ripe.prototype.accountMeP = function(options) {
     return new Promise((resolve, reject) => {
-        this.me(options, (result, isValid, request) => {
+        this.accountMe(options, (result, isValid, request) => {
             isValid ? resolve(result) : reject(new ripe.RemoteError(request));
         });
     });
