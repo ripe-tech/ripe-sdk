beee758537602cc5670f009b8f280d34cd8f7b30
João Magalhães <joamag@gmail.com>
2020-09-05 00:23:27 +0100

    Separated PRC from the base configurator instance

--- src/js/visual/index.js
+++ src/js/visual/index.js
@@ -1,7 +1,9 @@
 const configurator = require("./configurator");
+const configuratorPrc = require("./configuratorPrc");
 const image = require("./image");
 const visual = require("./visual");
 
 Object.assign(module.exports, configurator);
+Object.assign(module.exports, configuratorPrc);
 Object.assign(module.exports, image);
 Object.assign(module.exports, visual);
