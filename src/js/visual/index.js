221aa8ebbca784e915efe43d5a59804a700f17d9
João Magalhães <joamag@gmail.com>
2020-09-05 10:08:25 +0100

    Initial csr code

--- src/js/visual/index.js
+++ src/js/visual/index.js
@@ -1,9 +1,11 @@
 const configurator = require("./configurator");
+const configuratorCsr = require("./configurator-csr");
 const configuratorPrc = require("./configurator-prc");
 const image = require("./image");
 const visual = require("./visual");
 
 Object.assign(module.exports, configurator);
+Object.assign(module.exports, configuratorCsr);
 Object.assign(module.exports, configuratorPrc);
 Object.assign(module.exports, image);
 Object.assign(module.exports, visual);
