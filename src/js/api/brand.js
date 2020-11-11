323ff7a4dd418bde2b94f637b68a8346126c4ce1
Margarida Silva <ms@platforme.com>
2020-11-11 14:51:38 +0000

    Update links to docs

--- src/js/api/brand.js
+++ src/js/api/brand.js
@@ -458,7 +458,7 @@ ripe.Ripe.prototype._getMeshOptions = function(options = {}) {
 };
 
 /**
- * @see {link https://docs.platforme.com/#product-endpoints-config}
+ * @see {link https://docs.platforme.com/#brand-endpoints-config-and-spec}
  * @ignore
  */
 ripe.Ripe.prototype._getConfigOptions = function(options = {}) {
@@ -489,7 +489,7 @@ ripe.Ripe.prototype._getConfigOptions = function(options = {}) {
 };
 
 /**
- * @see {link https://docs.platforme.com/#product-endpoints-defaults}
+ * @see {link https://docs.platforme.com/#brand-endpoints-defaults}
  * @ignore
  */
 ripe.Ripe.prototype._getDefaultsOptions = function(options = {}) {
