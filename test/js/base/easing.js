9fb975ed2a493dd10881a17ba75afc06dff0dece
João Magalhães <joamag@gmail.com>
2020-11-11 20:04:21 +0000

    Added more easing unit tests

--- test/js/base/easing.js
+++ test/js/base/easing.js
@@ -37,4 +37,40 @@ describe("Easing", function() {
             assert.strictEqual(result, 4.375);
         });
     });
+
+    describe("#easeOutQuad()", async function() {
+        it("should be able to do some ease out quad easing", () => {
+            let result;
+
+            result = ripe.ripe.easing.easeOutQuad(0.5, 0, 10);
+            assert.strictEqual(result, 2.5);
+
+            result = ripe.ripe.easing.easeOutQuad(0, 0, 10);
+            assert.strictEqual(result, 0);
+
+            result = ripe.ripe.easing.easeOutQuad(1, 0, 10);
+            assert.strictEqual(result, 10);
+
+            result = ripe.ripe.easing.easeOutQuad(0.25, 0, 10);
+            assert.strictEqual(result, 0.625);
+        });
+    });
+
+    describe("#easeInOutQuad()", async function() {
+        it("should be able to do some ease in out quad easing", () => {
+            let result;
+
+            result = ripe.ripe.easing.easeInOutQuad(0.5, 0, 10);
+            assert.strictEqual(result, 5);
+
+            result = ripe.ripe.easing.easeInOutQuad(0, 0, 10);
+            assert.strictEqual(result, 0);
+
+            result = ripe.ripe.easing.easeInOutQuad(1, 0, 10);
+            assert.strictEqual(result, 10);
+
+            result = ripe.ripe.easing.easeInOutQuad(0.25, 0, 10);
+            assert.strictEqual(result, 1.25);
+        });
+    });
 });
