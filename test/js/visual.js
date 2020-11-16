fa83243e685bc59e06661650eef0cebb12136c6c
Margarida Silva <ms@platforme.com>
2020-11-16 11:20:20 +0000

    FIx test by awaiting sdk instance to load config before binding image

--- test/js/visual.js
+++ test/js/visual.js
@@ -49,13 +49,12 @@ describe("Visual", function() {
             assert.strictEqual(imageElement.src.includes("frame=side-10"), true);
         });
 
-        it("should generate the correct image URL", () => {
+        it("should generate the correct image URL", async () => {
             const dom = new jsdom.JSDOM("<img id='image' />");
             const imageElement = dom.window.document.getElementById("image");
             const instance = new base.ripe.Ripe("myswear", "vyner");
+            await instance.isReady();
             const image = instance.bindImage(imageElement, {
-                brand: "myswear",
-                model: "vyner",
                 rotation: 20,
                 flip: true,
                 mirror: true,
@@ -97,11 +96,10 @@ describe("Visual", function() {
                 ]
             });
             assert.strictEqual(image._url, null);
-
             image.update();
             assert.strictEqual(
                 image._url,
-                "https://sandbox.platforme.com/api/compose?algorithm=mask_top&background=ff00ff&bounding_box=2000&bounding_box=2000&curve=%5B%5B0.2%2C0.2%5D%2C%5B0.7%2C0.2%5D%2C%5B0.2%2C0.5%5D%2C%5B0.7%2C0.5%5D%5D&debug=true&engine=base&flip=true&font_size=50&font_spacing=10&font_trim=true&font_weight=900&initials_algorithm=multiplicative&initials_align=left&initials_blend_color=222222&initials_color=ff0000&initials_height=20&initials_image_flip=true&initials_image_rotation=30&initials_opacity=0.7&initials_rotation=20&initials_vertical=top&initials_viewport=1&initials_viewport=1&initials_viewport=20&initials_viewport=20&initials_width=20&initials_x=2&initials_y=2&initials_z_index=5&line_height=30&mirror=true&offsets=%7B%220%22%3A%5B0%2C10%5D%2C%221%22%3A%5B-1%2C-10%5D%7D&rotation=20&shadow=true&shadow_color=0000ff&shadow_offset=20"
+                "https://sandbox.platforme.com/api/compose?algorithm=mask_top&background=ff00ff&bounding_box=2000&bounding_box=2000&brand=myswear&curve=%5B%5B0.2%2C0.2%5D%2C%5B0.7%2C0.2%5D%2C%5B0.2%2C0.5%5D%2C%5B0.7%2C0.5%5D%5D&debug=true&engine=base&flip=true&font_size=50&font_spacing=10&font_trim=true&font_weight=900&initials_algorithm=multiplicative&initials_align=left&initials_blend_color=222222&initials_color=ff0000&initials_height=20&initials_image_flip=true&initials_image_rotation=30&initials_opacity=0.7&initials_rotation=20&initials_vertical=top&initials_viewport=1&initials_viewport=1&initials_viewport=20&initials_viewport=20&initials_width=20&initials_x=2&initials_y=2&initials_z_index=5&line_height=30&mirror=true&model=vyner&offsets=%7B%220%22%3A%5B0%2C10%5D%2C%221%22%3A%5B-1%2C-10%5D%7D&p=lining%3Acalf_lining%3Awhite&p=front%3Anappa%3Awhite&p=side%3Anappa%3Awhite&p=sole%3Arubber%3Awhite&p=laces%3Anylon%3Awhite&p=eyelets%3Ametal%3Asilver&p=shadow%3Adefault%3Adefault&rotation=20&shadow=true&shadow_color=0000ff&shadow_offset=20"
             );
         });
 
