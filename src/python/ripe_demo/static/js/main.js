25415ea5d239dc5d11607956a7f62545c82a611c
João Magalhães <joamag@gmail.com>
2020-12-02 08:05:28 +0000

    Fixed issue with demo code

--- src/python/ripe_demo/static/js/main.js
+++ src/python/ripe_demo/static/js/main.js
@@ -202,13 +202,17 @@ window.onload = function() {
                 });
             });
 
-        if (ripe.isOAuthPending()) {
-            ripe.oauth();
-        }
-
         oauthLogin = oauthLogin || { style: {} };
         oauthLogout = oauthLogout || { style: {} };
         oauthOperation = oauthOperation || { style: {} };
+
+        oauthLogin.style.display = "block";
+        oauthLogout.style.display = "none";
+        oauthOperation.style.display = "none";
+
+        if (ripe.isOAuthPending()) {
+            ripe.oauth();
+        }
 
         ripe.bind("auth", function() {
             oauthLogin.style.display = "none";
@@ -221,10 +225,6 @@ window.onload = function() {
             oauthLogout.style.display = "none";
             oauthOperation.style.display = "none";
         });
-
-        oauthLogin.style.display = "block";
-        oauthLogout.style.display = "none";
-        oauthOperation.style.display = "none";
     };
 
     var initConfigurator = function() {
