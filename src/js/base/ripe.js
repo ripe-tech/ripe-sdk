772bffc16d569e203fe762c967c092c9d9161fd8
João Magalhães <joamag@gmail.com>
2021-01-15 19:55:35 +0000

    version: 1.23.0

--- src/js/base/ripe.js
+++ src/js/base/ripe.js
@@ -16,7 +16,7 @@ if (
  * The version of the RIPE SDK currently in load, should
  * be in sync with the package information.
  */
-ripe.VERSION = "1.22.0";
+ripe.VERSION = "1.23.0";
 
 /**
  * Object that contains global (static) information to be used by
