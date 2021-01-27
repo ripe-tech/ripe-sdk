fec86c07719ea6f6a291ef15141eef4f2fe364ea
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-01-27 15:45:37 +0000

    feat: import order option types

--- types/base/ripe.d.ts
+++ types/base/ripe.d.ts
@@ -7,20 +7,38 @@ type Part = {
 type Spec = {
     brand: string;
     model: string;
-    parts: Array<Part>,
+    parts: Array<Part>;
     initials: string;
-    engraving: string,
-    initials_extra: object
+    engraving: string;
+    initialsExtra: object;
     variant?: string;
     version?: string;
     description?: string;
 };
+
+type ProductionOrderOptions = {
+    brand: string;
+    model: string;
+    variant: string;
+    parts: Array<Part>;
+    initials: string;
+    engraving: string;
+    initialsExtra: object;
+    gender: string;
+    size: string;
+    pending: boolean;
+    notify: boolean;
+    productId: string;
+    currency: string;
+    country: string;
+    meta: Array<string>;
+}
 
 export declare class Ripe {
     static id: number;
     static VERSION: number;
 
     constructor(brand?: string, model?: string, options?: any);
-    importOrderP(ffOrderId: string, options?: object, callback?: Function): Promise<void>;
+    importOrderP(ffOrderId: string, options?: ProductionOrderOptions, callback?: Function): Promise<void>;
     _queryToSpec(query: string): Spec;
 }
