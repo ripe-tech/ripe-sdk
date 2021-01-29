7f0daed5d02dfb63f77bbc36330367880c22384c
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-01-29 17:22:02 +0000

    feat: aggregate type and domains in own files

--- types/api/index.d.ts
+++ types/api/index.d.ts
@@ -1,1 +1,3 @@
+export * from "./attachment";
+export * from "./gender";
 export * from "./order";
--- types/api/order.d.ts
+++ types/api/order.d.ts
@@ -1,5 +1,86 @@
 import { Part } from "../base";
 
+import { Attachment } from "./attachment";
+import { Gender } from "./gender";
+
+export type Order = {
+    id: number;
+    number: number;
+    brand_t?: string;
+    retailer_t?: string;
+    vendor_t?: string;
+    key?: number;
+    status: OrderStatus;
+    status_index: number;
+    production_status?: OrderProductionStatus;
+    production?: string;
+    gender: Gender;
+    currency?: string;
+    country?: string;
+    price?: number;
+    brand: string;
+    shoe: string;
+    variant?: string;
+    shoe_query?: string;
+    shoe_url: string;
+    image_url?: string;
+    minify_hash?: string;
+    image_hash?: string;
+    image_o_hash?: string;
+    top_hash?: string;
+    top_o_hash?: string;
+    bottom_hash?: string;
+    bottom_o_hash?: string;
+    back_hash?: string;
+    side_hash?: string;
+    structure: string;
+    ff_id?: number;
+    ff_shoe_id?: number;
+    ff_order_id?: string;
+    delivery_time?: number;
+    tracking_number?: string;
+    tracking_url?: string;
+    report_url_e?: string;
+    notes?: string[];
+    prices?: {
+        components: Record<string, ComponentPrice>;
+        total: TotalPrice
+    };
+    states?: OrderState[];
+    attachments?: Attachment[];
+    account?: string;
+    customer?: string;
+    ambassador?: string;
+    store?: string;
+    number_s?: string;
+    channel?: string;
+    delivery_d?: number;
+    scale?: string;
+    size_scaled?: number;
+    details?: OrderDetails;
+}
+
+export enum OrderStatus {
+    pending = "pending",
+    created = "created",
+    production = "production",
+    ready = "ready",
+    sent = "sent",
+    received = "received",
+    returned = "returned",
+    canceled = "canceled"
+}
+
+export enum OrderProductionStatus {
+    unset = "unset",
+    waiting = "waiting",
+    cutting = "cutting",
+    sewing = "sewing",
+    assembling = "assembling",
+    produced = "produced",
+    canceled = "canceled"
+}
+
 export type OrderState = {
     id: number;
     enabled: boolean;
@@ -16,12 +97,21 @@ export type OrderState = {
     email: string;
 }
 
-export type Attachment = {
-    name: string;
-    key?: string;
-    kind: string;
-    content_type?: string;
-    url?: string;
+export type OrderDetails = {
+    brand: string;
+    model: string;
+    variant?: string;
+    parts: Part[];
+    parts_m: Record<string, {
+        material: string;
+        color: string;
+    }>;
+    gender: string;
+    size: number;
+    query: string;
+    url: string;
+    image: string;
+    extras: number;
 }
 
 export type ComponentPrice = {
@@ -82,106 +172,4 @@ export type TotalPrice = {
         exchanged: boolean;
     },
     cites: boolean;
-}
-
-export type OrderDetails = {
-    brand: string;
-    model: string;
-    variant?: string;
-    parts: Part[];
-    parts_m: Record<string, {
-        material: string;
-        color: string;
-    }>;
-    gender: string;
-    size: number;
-    query: string;
-    url: string;
-    image: string;
-    extras: number;
-}
-
-export enum OrderStatus {
-    pending = "pending",
-    created = "created",
-    production = "production",
-    ready = "ready",
-    sent = "sent",
-    received = "received",
-    returned = "returned",
-    canceled = "canceled"
-}
-
-export enum OrderProductionStatus {
-    unset = "unset",
-    waiting = "waiting",
-    cutting = "cutting",
-    sewing = "sewing",
-    assembling = "assembling",
-    produced = "produced",
-    canceled = "canceled"
-}
-
-export enum Gender {
-    male = "male",
-    female = "female",
-    kids = "kids",
-    unknown = "unknown"
-}
-
-export type Order = {
-    id: number;
-    number: number;
-    brand_t?: string;
-    retailer_t?: string;
-    vendor_t?: string;
-    key?: number;
-    status: OrderStatus;
-    status_index: OrderProductionStatus;
-    production_status?: OrderProductionStatus;
-    production?: string;
-    gender: Gender;
-    currency?: string;
-    country?: string;
-    price?: number;
-    brand: string;
-    shoe: string;
-    variant?: string;
-    shoe_query?: string;
-    shoe_url: string;
-    image_url?: string;
-    minify_hash?: string;
-    image_hash?: string;
-    image_o_hash?: string;
-    top_hash?: string;
-    top_o_hash?: string;
-    bottom_hash?: string;
-    bottom_o_hash?: string;
-    back_hash?: string;
-    side_hash?: string;
-    structure: string;
-    ff_id?: number;
-    ff_shoe_id?: number;
-    ff_order_id?: string;
-    delivery_time?: number;
-    tracking_number?: string;
-    tracking_url?: string;
-    report_url_e?: string;
-    notes?: string[];
-    prices?: {
-        components: Record<string, ComponentPrice>;
-        total: TotalPrice
-    };
-    states?: OrderState[];
-    attachments?: Attachment[];
-    account?: string;
-    customer?: string;
-    ambassador?: string;
-    store?: string;
-    number_s?: string;
-    channel?: string;
-    delivery_d?: number;
-    scale?: string;
-    size_scaled?: number;
-    details?: OrderDetails;
 }
