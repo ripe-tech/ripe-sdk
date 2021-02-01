e86b4bd682b175139e0add77c63699c2dbb58e3c
BeeMargarida <ms@platforme.com>
2021-02-01 14:07:57 +0000

    test: _queryToSpec test for initials extra with only one group

--- test/js/base/api.js
+++ test/js/base/api.js
@@ -63,6 +63,27 @@ describe("RipeAPI", function() {
                 initials_extra: {}
             });
         });
+
+        it("should be able to convert a query to spec with initials extra with one group", async () => {
+            const remote = ripe.RipeAPI();
+
+            const spec = remote._queryToSpec(
+                "brand=dummy&model=dummy&p=piping:leather_dmy:black&initials_extra=main:AA:black:style"
+            );
+            assert.deepStrictEqual(spec, {
+                brand: "dummy",
+                model: "dummy",
+                parts: {
+                    piping: {
+                        material: "leather_dmy",
+                        color: "black"
+                    }
+                },
+                initials: null,
+                engraving: null,
+                initials_extra: { main: { initials: "AA", engraving: "black:style" } }
+            });
+        });
     });
 
     describe("#_buildQuery()", function() {
