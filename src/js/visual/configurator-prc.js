dd726fdffb4fd96a928b7b3c846a86e6b7da184b
João Magalhães <joamag@gmail.com>
2021-02-18 16:00:48 +0000

    fix: add new line

--- src/js/visual/configurator-prc.js
+++ src/js/visual/configurator-prc.js
@@ -1378,6 +1378,7 @@ ripe.ConfiguratorPrc.prototype._preload = async function(useChain) {
             const _index = this.index;
             if (index !== _index) return;
             if (!this.element) return;
+
             // removes the preloading class from the image element
             // this is considered the default operation
             else element.classList.remove("preloading");
