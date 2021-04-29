cf2467fc1cb796b81c5b0d50aa59d2a957064225
BeeMargarida <ms@platforme.com>
2021-04-29 09:54:31 +0100

    style: newline

--- src/js/visual/configurator-prc.js
+++ src/js/visual/configurator-prc.js
@@ -1201,6 +1201,7 @@ ripe.ConfiguratorPrc.prototype._loadFrame = async function(view, position, optio
 
     const pending = framesBuffer.querySelectorAll("img.preloading") || [];
     if (pending.length === 0 && this._finalize) this._finalize();
+
     // triggers the post frame event indicating that the
     // frame has been buffered into the img element with
     // no cache operation activated
