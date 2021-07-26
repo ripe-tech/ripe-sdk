d8d17fdf6d693d5bfdd7a2a4d1eecfff77c5d401
JoaoLeal12 <joaofsl1994@gmail.com>
2021-07-26 10:34:58 +0100

    version: 2.2.2

--- src/js/base/ripe.js
+++ src/js/base/ripe.js
@@ -16,7 +16,7 @@ if (
  * The version of the RIPE SDK currently in load, should
  * be in sync with the package information.
  */
-ripe.VERSION = "2.2.1";
+ripe.VERSION = "2.2.2";
 
 /**
  * Object that contains global (static) information to be used by
