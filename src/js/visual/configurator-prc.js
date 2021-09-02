62030b35c6be0a61cb8c44b9ac75767755428f2e
Margarida Silva <ms@platforme.com>
2021-09-02 16:09:40 +0100

    feat: disable masks if build contains "no_masks" tag

--- src/js/visual/configurator-prc.js
+++ src/js/visual/configurator-prc.js
@@ -173,6 +173,8 @@ ripe.ConfiguratorPrc.prototype.updateOptions = async function(options, update = 
         options.configAnimate === undefined ? this.configAnimate : options.configAnimate;
     this.viewAnimate = options.viewAnimate === undefined ? this.viewAnimate : options.viewAnimate;
 
+    this.useMasks = this.owner.brand && this.owner.hasTag("no_masks") ? false : this.useMasks;
+
     if (update) await this.update();
 };
 
@@ -1027,6 +1029,8 @@ ripe.ConfiguratorPrc.prototype._updateConfig = async function(animate) {
     // in case the element is no longer available (possible due to async
     // nature of execution) returns the control flow immediately
     if (!this.element) return;
+
+    this.useMasks = this.owner.brand && this.owner.hasTag("no_masks") ? false : this.useMasks;
 
     // sets ready to false to temporarily block
     // update requests while the new config
