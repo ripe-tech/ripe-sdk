326dc655d4297442c7f0324feb1fa0aada478008
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-22 20:12:35 +0000

    test: update tests

--- test/js/api/shipment.js
+++ test/js/api/shipment.js
@@ -28,6 +28,64 @@ describe("ShipmentAPI", function() {
         });
     });
 
+    describe("#getShipment()", function() {
+        beforeEach(function() {
+            if (!config.TEST_USERNAME || !config.TEST_PASSWORD) {
+                this.skip();
+            }
+        });
+
+        it("should be able to retrieve a shipment", async () => {
+            let result = null;
+
+            const remote = ripe.RipeAPI();
+
+            result = await remote.authAdminP(config.TEST_USERNAME, config.TEST_PASSWORD);
+
+            assert.strictEqual(result.username, config.TEST_USERNAME);
+            assert.notStrictEqual(typeof result.sid, undefined);
+
+            const shipment = await remote.createShipmentP({
+                status: "sent",
+                order: 2,
+                courier: "ups",
+                service: "airplane",
+                package: "envelope",
+                tracking_number: "tracking123",
+                tracking_url: "http://platforme.com/tracking/ups/tracking123",
+                shipping_date: Date.now(),
+                delivery_date: Date.now(),
+                shipper: {
+                    name: "Name",
+                    phone: "919999999",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        country_code: "PT"
+                    }
+                },
+                customer: {
+                    name: "Name",
+                    email: "email@email.com",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        state_code: "NY",
+                        country_code: "US"
+                    }
+                }
+            });
+
+            result = await remote.getShipmentP(shipment.number);
+
+            await remote.deleteShipmentP(result.number);
+
+            assert.strictEqual(result, shipment);
+        });
+    });
+
     describe("#createShipment()", function() {
         beforeEach(function() {
             if (!config.TEST_USERNAME || !config.TEST_PASSWORD) {
@@ -46,33 +104,161 @@ describe("ShipmentAPI", function() {
 
             const now = Date.now();
             result = await remote.createShipmentP({
-                status: "created",
-                brand: "dummy",
+                status: "sent",
+                order: 2,
+                description: "A test shipment",
+                weight: 1.5,
+                weight_units: "kilograms",
                 courier: "ups",
+                service: "airplane",
+                package: "envelope",
                 tracking_number: "tracking123",
                 tracking_url: "http://platforme.com/tracking/ups/tracking123",
+                carbon_neutral: true,
+                access_point_delivery: "optional",
+                pickup: {
+                    offset: 1000,
+                    ready_time: 32400000,
+                    close_time: 64800000
+                },
                 shipping_date: now,
                 delivery_date: now,
-                origin_country: "PT",
-                origin_city: "Porto",
-                destination_country: "GB",
-                destination_city: "London"
+                shipper: {
+                    name: "Name",
+                    phone: "919999999",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        country_code: "PT"
+                    }
+                },
+                customer: {
+                    name: "Name",
+                    email: "email@email.com",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        state_code: "NY",
+                        country_code: "US"
+                    }
+                },
+                attachments: [1, 2, 3]
             });
 
             assert.strictEqual(result.status, "created");
-            assert.strictEqual(result.brand, "dummy");
+            assert.strictEqual(result.order, 2);
+            assert.strictEqual(result.description, "A test shipment");
+            assert.strictEqual(result.weight, 1.5);
+            assert.strictEqual(result.weight_units, "kilograms");
             assert.strictEqual(result.courier, "ups");
+            assert.strictEqual(result.service, "airplane");
+            assert.strictEqual(result.package, "envelope");
             assert.strictEqual(result.tracking_number, "tracking123");
             assert.strictEqual(
                 result.tracking_url,
                 "http://platforme.com/tracking/ups/tracking123"
             );
+            assert.strictEqual(result.carbon_neutral, true);
+            assert.strictEqual(result.access_point_delivery, "optional");
+            assert.strictEqual(result.pickup, {
+                offset: 1000,
+                ready_time: 32400000,
+                close_time: 64800000
+            });
             assert.strictEqual(result.shipping_date, now);
             assert.strictEqual(result.delivery_date, now);
-            assert.strictEqual(result.origin_country, "PT");
-            assert.strictEqual(result.origin_city, "Porto");
-            assert.strictEqual(result.destination_country, "GB");
-            assert.strictEqual(result.destination_city, "London");
+            assert.strictEqual(result.shipper, {
+                name: "Name",
+                phone: "919999999",
+                address: {
+                    line: "Address Line",
+                    city: "City",
+                    postal_code: "4475852",
+                    country_code: "PT"
+                }
+            });
+            assert.strictEqual(result.customer, {
+                name: "Name",
+                email: "email@email.com",
+                address: {
+                    line: "Address Line",
+                    city: "City",
+                    postal_code: "4475852",
+                    state_code: "NY",
+                    country_code: "US"
+                }
+            });
+            assert.strictEqual(result.attachments, [1, 2, 3]);
+
+            await remote.deleteShipmentP(result.number);
+        });
+    });
+
+    describe("#updateShipment()", function() {
+        beforeEach(function() {
+            if (!config.TEST_USERNAME || !config.TEST_PASSWORD) {
+                this.skip();
+            }
+        });
+
+        it("should be able to update a shipment", async () => {
+            let result = null;
+
+            const remote = ripe.RipeAPI();
+            result = await remote.authAdminP(config.TEST_USERNAME, config.TEST_PASSWORD);
+
+            assert.strictEqual(result.username, config.TEST_USERNAME);
+            assert.notStrictEqual(typeof result.sid, undefined);
+
+            const shipment = await remote.createShipmentP({
+                status: "sent",
+                order: 2,
+                courier: "ups",
+                service: "airplane",
+                package: "envelope",
+                tracking_number: "tracking123",
+                tracking_url: "http://platforme.com/tracking/ups/tracking123",
+                shipping_date: Date.now(),
+                delivery_date: Date.now(),
+                shipper: {
+                    name: "Name",
+                    phone: "919999999",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        country_code: "PT"
+                    }
+                },
+                customer: {
+                    name: "Name",
+                    email: "email@email.com",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        state_code: "NY",
+                        country_code: "US"
+                    }
+                }
+            });
+
+            result = await remote.updateShipmentP({
+                number: shipment.number,
+                courier: "dhl",
+                service: "express",
+                package: "customer_box",
+                shipper: {
+                    name: "Updated Name"
+                }
+            });
+
+            assert.strictEqual(result.courier, "dhl");
+            assert.strictEqual(result.service, "express");
+            assert.strictEqual(result.package, "customer_box");
+            assert.strictEqual(result.shipper.name, "Updated Name");
 
             await remote.deleteShipmentP(result.number);
         });
@@ -95,19 +281,37 @@ describe("ShipmentAPI", function() {
             assert.strictEqual(result.username, config.TEST_USERNAME);
             assert.notStrictEqual(typeof result.sid, undefined);
 
-            const now = Date.now();
             const shipment = await remote.createShipmentP({
-                status: "created",
-                brand: "dummy",
+                status: "sent",
+                order: 2,
                 courier: "ups",
+                service: "airplane",
+                package: "envelope",
                 tracking_number: "tracking123",
                 tracking_url: "http://platforme.com/tracking/ups/tracking123",
-                shipping_date: now,
-                delivery_date: now,
-                origin_country: "PT",
-                origin_city: "Porto",
-                destination_country: "GB",
-                destination_city: "London"
+                shipping_date: Date.now(),
+                delivery_date: Date.now(),
+                shipper: {
+                    name: "Name",
+                    phone: "919999999",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        country_code: "PT"
+                    }
+                },
+                customer: {
+                    name: "Name",
+                    email: "email@email.com",
+                    address: {
+                        line: "Address Line",
+                        city: "City",
+                        postal_code: "4475852",
+                        state_code: "NY",
+                        country_code: "US"
+                    }
+                }
             });
 
             result = await remote.deleteShipmentP(shipment.number);
