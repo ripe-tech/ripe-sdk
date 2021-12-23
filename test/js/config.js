acd72607366edb492148a44b494d29be9904b61e
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-23 15:50:39 +0000

    feat: add test url

--- test/js/config.js
+++ test/js/config.js
@@ -1,4 +1,5 @@
 const TEST_TIMEOUT = process.env.TEST_TIMEOUT ? parseInt(process.env.TEST_TIMEOUT) : 30000;
+const TEST_URL = process.env.TEST_URL || "https://ripe-core-sbx.platforme.com/api/";
 const TEST_USERNAME = process.env.TEST_USERNAME || null;
 const TEST_PASSWORD = process.env.TEST_PASSWORD || null;
 const TEST_KEY = process.env.TEST_KEY || null;
@@ -6,6 +7,7 @@ const SKIP_TEST_NOTIFY = process.env.SKIP_TEST_NOTIFY ? parseInt(process.env.SKI
 
 module.exports = {
     TEST_TIMEOUT: TEST_TIMEOUT,
+    TEST_URL: TEST_URL,
     TEST_USERNAME: TEST_USERNAME,
     TEST_PASSWORD: TEST_PASSWORD,
     TEST_KEY: TEST_KEY,
