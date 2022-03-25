8f1b7e631f02f987b7e829d46e377377dd497b0f
3rdvision <kazgru@gmail.com>
2022-03-25 14:11:48 +0000

    Revert "refactor: small simplification"
    This reverts commit ee211f1dbd6e5b278033af4f63910e6ff22a1c38.

--- src/js/visual/image.js
+++ src/js/visual/image.js
@@ -104,7 +104,7 @@ ripe.Image.prototype.init = function() {
     this.curve = this.options.curve || null;
     this.showInitials = this.options.showInitials || false;
     this.remoteInitialsBuilderLogic =
-        this.options.remoteInitialsBuilderLogic || this.owner.remoteInitialsBuilderLogic || null;
+        this.options.remoteInitialsBuilderLogic || this.owner.remoteInitialsBuilderLogic || false;
     this.initialsGroup = this.options.initialsGroup || null;
     this.initialsContext = this.options.initialsContext || null;
     this.getInitialsContext = this.options.getInitialsContext || null;
@@ -328,7 +328,9 @@ ripe.Image.prototype.update = async function(state, options = {}) {
         const curve = this.element.dataset.curve || this.curve;
         const doubleBuffering = this.element.dataset.doubleBuffering || this.doubleBuffering;
         const remoteInitialsBuilderLogic =
-            this.element.dataset.remoteInitialsBuilderLogic || this.remoteInitialsBuilderLogic;
+            this.element.dataset.remoteInitialsBuilderLogic ||
+            this.remoteInitialsBuilderLogic ||
+            undefined;
 
         // in case the state is defined tries to gather the appropriate
         // sate options for both initials and engraving taking into
