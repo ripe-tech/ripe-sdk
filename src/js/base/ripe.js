36b68030e9f82db411f155eadcf754e0c1b83995
Margarida Silva <ms@platforme.com>
2022-03-31 10:17:28 +0100

    version: 2.25.1

--- src/js/base/ripe.js
+++ src/js/base/ripe.js
@@ -16,7 +16,7 @@ if (
  * The version of the RIPE SDK currently in load, should
  * be in sync with the package information.
  */
-ripe.VERSION = "2.25.0";
+ripe.VERSION = "2.25.1";
 
 /**
  * Object that contains global (static) information to be used by
