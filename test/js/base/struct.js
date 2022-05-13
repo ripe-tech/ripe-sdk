589f0f99c4b941dac25dc16f47e36dce2f5bd7c9
NFSS10 <nuno_nfss96@hotmail.com>
2022-05-13 15:57:20 +0100

    feat: add  `#fromString()`, `#fromArrayBuffer()` and `#fromBlob()` tests

--- test/js/base/struct.js
+++ test/js/base/struct.js
@@ -2,6 +2,8 @@ const assert = require("assert");
 const config = require("../config");
 const ripe = require("../../../src/js");
 
+const { Blob } = require("buffer");
+
 describe("FileTuple", function() {
     this.timeout(config.TEST_TIMEOUT);
 
@@ -32,6 +34,56 @@ describe("FileTuple", function() {
             assert.strictEqual(fileTuple.data().byteLength, 0);
             assert.strictEqual(fileTuple instanceof ripe.ripe.FileTuple, true);
             assert.strictEqual(fileTuple instanceof Array, true);
+        });
+    });
+
+    describe("#fromString()", async function() {
+        it("should be able to create a simple file tuple objects", () => {
+            const fileTuple = ripe.ripe.FileTuple.fromString("hello", "hello.txt", "text/plain");
+            assert.notStrictEqual(fileTuple, null);
+            assert.strictEqual(fileTuple.constructor, ripe.ripe.FileTuple);
+            assert.strictEqual(fileTuple.name, "hello.txt");
+            assert.strictEqual(fileTuple.mime, "text/plain");
+            assert.strictEqual(fileTuple.data.byteLength, 5);
+            assert.strictEqual(fileTuple instanceof ripe.ripe.FileTuple, true);
+            assert.strictEqual(fileTuple instanceof Array, true);
+            assert.strictEqual(Array.isArray(fileTuple), true);
+        });
+    });
+
+    describe("#fromArrayBuffer()", async function() {
+        it("should be able to create a simple file tuple objects", () => {
+            const fileTuple = ripe.ripe.FileTuple.fromArrayBuffer(
+                new ArrayBuffer(),
+                "hello.txt",
+                "text/plain"
+            );
+            assert.notStrictEqual(fileTuple, null);
+            assert.strictEqual(fileTuple.constructor, ripe.ripe.FileTuple);
+            assert.strictEqual(fileTuple.name, "hello.txt");
+            assert.strictEqual(fileTuple.mime, "text/plain");
+            assert.strictEqual(fileTuple.data.byteLength, 0);
+            assert.strictEqual(fileTuple instanceof ripe.ripe.FileTuple, true);
+            assert.strictEqual(fileTuple instanceof Array, true);
+            assert.strictEqual(Array.isArray(fileTuple), true);
+        });
+    });
+
+    describe("#fromBlob()", async function() {
+        it("should be able to create a simple file tuple objects", async () => {
+            const fileTuple = await ripe.ripe.FileTuple.fromBlob(
+                new Blob(),
+                "hello.txt",
+                "text/plain"
+            );
+            assert.notStrictEqual(fileTuple, null);
+            assert.strictEqual(fileTuple.constructor, ripe.ripe.FileTuple);
+            assert.strictEqual(fileTuple.name, "hello.txt");
+            assert.strictEqual(fileTuple.mime, "text/plain");
+            assert.strictEqual(fileTuple.data.byteLength, 0);
+            assert.strictEqual(fileTuple instanceof ripe.ripe.FileTuple, true);
+            assert.strictEqual(fileTuple instanceof Array, true);
+            assert.strictEqual(Array.isArray(fileTuple), true);
         });
     });
 });
