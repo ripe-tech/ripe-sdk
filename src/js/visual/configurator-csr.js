d5e3bf80229968e052b346e2eefc6e6f6e37dff8
Nuno Silva <nuno_nfss96@hotmail.com>
2022-08-31 10:30:24 +0100

    faet: add vertical drag

--- src/js/visual/configurator-csr.js
+++ src/js/visual/configurator-csr.js
@@ -110,6 +110,7 @@ ripe.ConfiguratorCsr.prototype.init = async function() {
     this.referenceX = null;
     this.referenceY = null;
     this.prevPercentX = 0;
+    this.prevPercentY = 0;
 
     // creates the necessary DOM elements and runs the
     // CSR initializer
@@ -483,18 +484,21 @@ ripe.ConfiguratorCsr.prototype._onMouseDown = function(self, event) {
     self.referenceX = event.pageX;
     self.referenceY = event.pageY;
     self.prevPercentX = 0;
+    self.prevPercentY = 0;
     console.log("mousedown:", self.referenceX, self.referenceY);
 };
 
 ripe.ConfiguratorCsr.prototype._onMouseUp = function(self, event) {
     self.isMouseDown = false;
     self.prevPercentX = 0;
+    self.prevPercentY = 0;
     console.log("mouseup:", event);
 };
 
 ripe.ConfiguratorCsr.prototype._onMouseLeave = function(self, event) {
     self.isMouseDown = false;
     self.prevPercentX = 0;
+    self.prevPercentY = 0;
     console.log("mouseleave:", event);
 };
 
@@ -512,14 +516,16 @@ ripe.ConfiguratorCsr.prototype._onMouseMove = function(self, event) {
     const elementHeight = self.element.clientHeight;
     const percentX = deltaX / elementWidth;
     const percentY = deltaY / elementHeight;
-
-    // TODO handle vertical movement
-
     const sensitivity = this.sensitivity * 0.1;
+
     const dragValueX = (percentX - this.prevPercentX) * sensitivity;
     self.mesh.rotation.y -= dragValueX;
 
+    const dragValueY = (percentY - this.prevPercentY) * sensitivity;
+    self.mesh.rotation.x -= dragValueY;
+
     this.prevPercentX = percentX;
+    this.prevPercentY = percentY;
 };
 
 /**
