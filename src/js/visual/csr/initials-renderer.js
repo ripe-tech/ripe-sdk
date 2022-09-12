df72b612f2779a3a50035f5ebcca4e9919565c04
Nuno Silva <nuno_nfss96@hotmail.com>
2022-09-12 15:24:09 +0100

    feat: create `CsrInitialsRenderer`

---
+++ src/js/visual/csr/initials-renderer.js
@@ -0,0 +1,19 @@
+if (
+    typeof require !== "undefined" &&
+    (typeof window === "undefined" ||
+        // eslint-disable-next-line camelcase
+        typeof __webpack_require__ !== "undefined" ||
+        (typeof navigator !== "undefined" && navigator.product === "ReactNative"))
+) {
+    // eslint-disable-next-line no-redeclare,no-var
+    var base = require("../../base");
+    // eslint-disable-next-line no-redeclare,no-var
+    var ripe = base.ripe;
+}
+
+ripe.CsrInitialsRenderer = function() {};
+ripe.CsrInitialsRenderer.prototype.constructor = ripe.CsrInitialsRenderer;
+
+ripe.CsrInitialsRenderer.prototype.destroy = function() {
+    // TODO
+};
--- src/js/visual/index.js
+++ src/js/visual/index.js
@@ -1,5 +1,6 @@
 const csrAnimation = require("./csr/animation-base");
 const csrChangeFrameAnimation = require("./csr/animation-change-frame");
+const csrInitialsRenderer = require("./csr/initials-renderer");
 const csrUtils = require("./csr/utils");
 const configuratorCsr = require("./configurator-csr");
 const configuratorPrc = require("./configurator-prc");
@@ -8,6 +9,7 @@ const visual = require("./visual");
 
 Object.assign(module.exports, csrAnimation);
 Object.assign(module.exports, csrChangeFrameAnimation);
+Object.assign(module.exports, csrInitialsRenderer);
 Object.assign(module.exports, csrUtils);
 Object.assign(module.exports, configuratorCsr);
 Object.assign(module.exports, configuratorPrc);
