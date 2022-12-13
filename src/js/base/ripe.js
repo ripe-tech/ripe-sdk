e0dcdf8cde050811c4d7441ef7b5cece6dab29a6
3rdvision <kazgru@gmail.com>
2022-12-13 12:52:59 +0000

    chore: version 2.33.0

--- src/js/base/ripe.js
+++ src/js/base/ripe.js
@@ -16,7 +16,7 @@ if (
  * The version of the RIPE SDK currently in load, should
  * be in sync with the package information.
  */
-ripe.VERSION = "2.31.2";
+ripe.VERSION = "2.33.0";
 
 /**
  * Object that contains global (static) information to be used by
