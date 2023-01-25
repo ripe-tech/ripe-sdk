37f81c46d221ff0b379ea0b2ff7888636457b395
Nuno Silva <nuno_nfss96@hotmail.com>
2023-01-25 15:36:16 +0000

    style: pretty

--- src/js/api/order.js
+++ src/js/api/order.js
@@ -2456,7 +2456,8 @@ ripe.Ripe.prototype._importOrder = function(ffOrderId, options = {}) {
     const notes = options.notes === undefined ? null : options.notes;
     const images = options.images === undefined ? null : options.images;
     const safe = options.safe === undefined ? null : options.safe ? "1" : "0";
-    const imagesDefault = options.imagesDefault === undefined ? null : options.imagesDefault ? 1 : 0;
+    const imagesDefault =
+        options.imagesDefault === undefined ? null : options.imagesDefault ? 1 : 0;
 
     const url = `${this.url}orders/import`;
     const contents = {};
